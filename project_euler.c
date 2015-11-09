1616d54ea91f778f8e0048f4347f45f53a6ff111
Thomas Elder <thomas.charles.elder@gmail.com>
2015-11-09 19:35:39 +1100

    Basic Menu implemented
    Created a basic menu for browsing solutions, so I can just add an option
    for each solution as they are created.

--- project_euler.c
+++ project_euler.c
@@ -4,9 +4,52 @@
 
 int main(void) {
 
-	printf("%s\n", "Welcome to Tom's Project Euler solutions!");
-	printf("%s\n", "Please select from the following solutions:");
+	/* local variables */
+	char buffer[256];
+	int input;
 
+	input = -1;
+
+	while (input != 0){
+
+		/* menu */
+		printf("%s\n", "Welcome to Tom's Project Euler solutions!");
+		printf("%s\n", "Please select from the following solutions:");
+		printf("%s\n", "1 - Multiples of 3 and 5");
+		printf("%s\n", "0 - Exit");
+
+		fgets(buffer, MAXINT, stdin);
+		read_rest_of_line();
+		input = atoi(buffer);
+
+		switch (input) {
+			case 1:
+				printf("%s\n", "Multiples of 3 and 5 not yet implemented.");
+				break;
+			case 0:
+				return 0;
+			default:
+				printf("%s\n", "Invalid input, try again.");
+				break;
+		}
+	}
 
 	return 0;
+}
+
+/* Function Definitions */
+void read_rest_of_line(void)
+{
+	/* local variables */
+    int ch;
+
+    /* read characters one at a time from standard input until there are
+     * no characters left to read
+     */
+    while (ch=getc(stdin), ch!=EOF && ch!='\n'){
+        printf("%s : %c\n", "Reading from buffer character", ch);
+    }
+    
+    /* reset the error status of the input FILE pointer */
+    clearerr(stdin);
 }
--- project_euler.h
+++ project_euler.h
@@ -3,7 +3,14 @@
 #ifndef PROJECT_EULER_H_INCLUDED
 #define PROJECT_EULER_H_INCLUDED
 
+/* includes */
 #include <stdio.h>
 #include <stdlib.h>
+
+/* definitions */
+#define MAXINT 2
+
+/* prototypes */
+void read_rest_of_line(void);
 
 #endif
