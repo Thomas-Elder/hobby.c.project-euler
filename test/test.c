7bbb73953d769e7bda0e045b5ea5027822422d75
Thomas Elder <thomas.charles.elder@gmail.com>
2016-07-14 21:06:19 +1000

    Implementing sum_even_fibonacci_numbers test #17
    Currently failing the test for a limit of 100. I'm expecting 44, given the
    even numbers below 100 in the sequence are 2, 8 and 34. But it's returning
    168.
    
    Oddly, it's passing for 50, and 1000.

--- test/test.c
+++ test/test.c
@@ -26,6 +26,15 @@ int main(void)
 
 	total_tests_passed += passed;
 
+	/* Testing sum_even_fibonacci_numbers function */
+	printf("%s\n", "Testing sum_even_fibonacci_numbers function... ");
+	passed = sum_even_fibonacci_numbers_all_tests();
+	printf("%s%d\n", "Tests out of 4 passed: ", passed);
+	printf("%s\n", "Testing sum_even_fibonacci_numbers complete.");
+	printf("%s\n", "");
+
+	total_tests_passed += passed;
+
 	/* Testing other functions */
 
 	/* Total test score */
@@ -84,6 +93,65 @@ int sum_multiples_all_tests(void){
 	/* test 4 */
 	expected = 105;
 	result = sum_multiples(3, 7, 23);
+
+	if (result != expected) {
+		printf("%s\n", "Test 4 Failed!");
+		printf("%s%d%s%d\n", "Expected: ", expected, ". Result: ", result);
+	} else {
+		tests_passed++;
+	}
+
+	return tests_passed;
+}
+
+/* sum_even_fibonacci_numbers_all_tests
+ *
+ */
+int sum_even_fibonacci_numbers_all_tests(void) {
+
+	/* local variables */
+	int tests_passed;
+	int expected;
+	int result;
+
+	tests_passed = 0;
+
+	/* test 1 */
+	expected = 4613732;
+	result = sum_even_fibonacci_numbers(4000000);
+
+	if (result != expected) {
+		printf("%s\n", "Test 1 Failed!");
+		printf("%s%d%s%d\n", "Expected: ", expected, ". Result: ", result);
+	} else {
+		tests_passed++;
+	}
+
+	/* test 2 */
+	expected = 44;
+	result = sum_even_fibonacci_numbers(50);
+
+	if (result != expected) {
+		printf("%s\n", "Test 2 Failed!");
+		printf("%s%d%s%d\n", "Expected: ", expected, ". Result: ", result);
+	} else {
+		tests_passed++;
+	}
+
+	/* test 3 */
+	expected = 44;
+	result = sum_even_fibonacci_numbers(100);
+
+	if (result != expected) {
+		printf("%s\n", "Test 3 Failed!");
+		printf("%s%d%s%d\n", "Expected: ", expected, ". Result: ", result);
+	} else {
+		tests_passed++;
+	}
+
+	/* test 4 */
+	expected = 798;
+	result = sum_even_fibonacci_numbers(1000);
 
 	if (result != expected) {
 		printf("%s\n", "Test 4 Failed!");
--- test/test.h
+++ test/test.h
@@ -36,8 +36,6 @@
  *
  * FUNCTION'S SOURCE : solutions.c
  *
- * FUNCTION TEST     : sum_multiples_test()
- *
  * TEST CASE #1: 
  * The Project Euler example values.
  *              PARAMETERS : int a = 3
@@ -47,25 +45,58 @@
  *
  * TEST CASE #2:
  * The actual Project Euler problem values. 
- *              PARAMETERS :int a = 3
- *                          int b = 5
- *                          int limit = 1000
+ *              PARAMETERS : int a = 3
+ *                           int b = 5
+ *                           int limit = 1000
  *              EXPECTED   : 233168
  *
  * TEST CASE #3: 
- *              PARAMETERS :int a = 4
- *                          int b = 6
- *                          int limit = 20
+ *              PARAMETERS : int a = 4
+ *                           int b = 6
+ *                           int limit = 20
  *              EXPECTED   : 76
  *
  * TEST CASE #4: 
- *              PARAMETERS :int a = 3
- *                          int b = 7
- *                          int limit = 23
+ *              PARAMETERS : int a = 3
+ *                           int b = 7
+ *                           int limit = 23
  *              EXPECTED   : 126
  *
  * NOTES      : 
  */
 int sum_multiples_all_tests(void);
+
+/* sum_even_fibonacci_numbers_all_tests
+ *
+ * PARAMETERS        : void
+ *
+ * RETURNS           : int
+ *                     An integer from 0-4, indicating how many
+ *                     of the 4 test cases were passed. 
+ *
+ * FUCTION TESTED    : sum_even_fibonacci_numbers()
+ *
+ * FUNCTION'S SOURCE : solutions.c
+ *
+ * TEST CASE #1: 
+ * The actual Project Euler problem value.
+ *              PARAMETERS : int limit = 4000000
+ *              EXPECTED   : 4613732
+ *
+ * TEST CASE #2:
+ *              PARAMETERS : int limit = 50
+ *              EXPECTED   : 44
+ *
+ * TEST CASE #3: 
+ *              PARAMETERS : int limit = 100
+ *              EXPECTED   : 44
+ *
+ * TEST CASE #4: 
+ *              PARAMETERS :int limit = 1000
+ *              EXPECTED   : 798
+ *
+ * NOTES      : 
+ */
+int sum_even_fibonacci_numbers_all_tests(void);
 
 #endif
